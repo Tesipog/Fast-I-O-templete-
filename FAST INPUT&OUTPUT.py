e0c91914a4455b73db247a45755ea03ef74154f6
Tesipog <95682499+Tesipog@users.noreply.github.com>
2022-07-24 17:41:17 +0530

    Modified

---
+++ FAST INPUT&OUTPUT.py
@@ -0,0 +1,61 @@
+import os  # interact with operating system
+import sys  # fast input,output ,maxsize etc
+from io import BytesIO, IOBase
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()  # returns integer file descriptor
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+inp = lambda: sys.stdin.readline().rstrip("\r\n")
+ri = lambda: int(sys.stdin.readline().rstrip("\r\n"))
+ris = lambda: map(int, sys.stdin.readline().rstrip("\r\n").split())
+rs = lambda: sys.stdin.readline().rstrip("\r\n")
+rss = lambda: map(str, sys.stdin.readline().rstrip("\r\n").split())
+ria = lambda: [int(_) for _ in sys.stdin.readline().rstrip("\r\n").split()]
+rsa = lambda: [str(_) for _ in sys.stdin.readline().rstrip("\r\n").split()]
+
+
